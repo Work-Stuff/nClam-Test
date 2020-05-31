5920646f84a7ab68d1b94f3e67f6c1efb287cbf4
Tom Needham <06needhamt@gmail.com>
2020-05-31 20:07:21 +0100

    Update Example to latest Clam

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using nClam;
+
+namespace ClamPing
+{
+    class Program
+    {
+        const string SERVER_URL = "localhost";
+        const int SERVER_PORT = 3310;
+        const string fileName = "test.mp4";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ClamClient client = new ClamClient(SERVER_URL, SERVER_PORT);
+            bool ping = Task.Run(() => client.PingAsync()).Result;
+
+            Console.WriteLine($"Ping Successful? {ping}");
+
+            ClamScanResult result = Task.Run(() => client.ScanFileOnServerAsync(Path.Combine(Environment.CurrentDirectory, fileName))).Result;
+            
+            Console.WriteLine($"Scan Successful? {result.RawResult}");
+
+        }
+    }
+}
